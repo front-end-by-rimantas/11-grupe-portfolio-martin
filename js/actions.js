f327699bf6ab15916942a6c7f82fbf2dbac85ceb
Doirena <barkauskaite.dovile@gmail.com>
2019-11-20 14:08:49 +0200

    add header button action js

--- js/actions.js
+++ js/actions.js
@@ -1,6 +1,35 @@
 "use strict";
 
-// header
+// header// header
+const hamburger = document.querySelector('#main_header .fa-bars');
+const close = document.querySelector('#main_header .fa-bars');
+const header = document.querySelector('#main_header');
+
+hamburger.addEventListener('click', ()=>{ 
+    header.classList.toggle('mobile-show');
+    // console.log('reikia ispasudinti meniu ');
+    return;
+});
+//tam paciam jei noretum, tai neina!!!
+// close.addEventListener('click', ()=>{
+// function closeMenu(){
+//     header.classList.toggle('mobile-show');
+//     return;
+// }
+
+//VIDINES RODYKLES ACTIVE:
+const angle = document.querySelector('.dropdown .fa-angle-down');
+
+// const close = document.querySelector('#main_header .fa-bars');
+const drop = document.querySelector('.dropdown');
+// console.log (drop);
+// console.log (angle);
+
+angle.addEventListener('click', ()=>{ 
+    drop.classList.toggle('show-angle');
+    // console.log('reikia ispasudinti meniu ');
+    return;
+});
 
 // hero
 
